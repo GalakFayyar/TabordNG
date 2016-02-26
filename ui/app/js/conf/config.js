8272cee27454b69c9fa4912e6398d7c5434cff8a
GalakFayyar <thomas.falcher@cgi.com>
2016-02-27 00:27:07 +0100

    Adjustment for python 2.7 use

--- ui/app/js/conf/config.js
+++ ui/app/js/conf/config.js
@@ -6,7 +6,7 @@
 	angular.module('tabordNG').constant('tabordngConfig', {
 		'version': '0.0.1',
 		'api': {
-			'url': 'http://localhost/tabord_ng/api/' //TODO : define url authentification api
+			'url': 'http://localhost:5000/api/' //TODO : define url authentification api
 		},
 		'activateLogging': true,
 		'timeoutAlertMessages': 5000
