bf5c066c2e05295b0ffd58df65ee606f37049820
GalakFayyar <thomas.falcher@cgi.com>
2016-06-26 17:40:03 +0200

    Add first form UI for merchandising

--- ui/app/js/controllers/MerchandisingController.js
+++ ui/app/js/controllers/MerchandisingController.js
@@ -10,5 +10,32 @@
 		$.AdminLTE.layout.activate();
 
 		ngProgress.color('#FFF');
+
+		setTimeout(function() {
+			$(":checkbox").labelauty();
+			$(":radio").labelauty();
+		});
+		
+		// Merchandising Form Values
+		$scope.form = {
+			pharmacie: {
+				code: null
+			},
+			contexteEnvironnemental: {
+				implantationVente: null,
+				visibiliteLocal: null,
+				accessibilite: null,
+				commoditeStationnement: null,
+				environnementCommercial: null,
+				fluxPietonnier: null,
+				fluxAutomobile: null,
+				longeurVitrine: null,
+				surface: null,
+				envMedicalGeneraliste: null,
+				envMedicalSpecialiste: null,
+				envCentresMedicaux: null,
+				envMedicalHospitalier: null
+			}
+		}
 	}
 })();
