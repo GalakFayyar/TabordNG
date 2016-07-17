6a928e237c6cb4b90802a58e8b8fee7318bf6314
Thomas Falcher <thomasfalcher@MacBookPro-2.local>
2016-07-17 13:14:20 +0200

    Delete old config file. Edit api adress (port)

--- ui/app/js/conf/config.js
+++ ui/app/js/conf/config.js
@@ -7,7 +7,7 @@
 		'version': '0.0.1',
 		'api': {
 			//'url': 'http://localhost:5000/api/' //TODO : define url authentification api
-			'url': 'http://91.121.133.203:5000/api/'
+			'url': 'http://91.121.133.203:8077/api/'
 		},
 		'activateLogging': true,
 		'timeoutAlertMessages': 5000
