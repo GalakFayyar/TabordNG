2fe9c2cc4e92108b7e2b737ab1aae795503574ca
Thomas Falcher <thomasfalcher@MacBookPro-2.local>
2016-09-26 22:22:26 +0200

    Add file converter UTF8 format

--- batchs/convert_utf8.sh
+++ batchs/convert_utf8.sh
@@ -3,9 +3,9 @@
 cd data
 for file in `*.tab`
 	do
-		iconv -f ISO-8859-1 $i -t UTF-8 -o $i_utf8.csv
-		rm $i.csv
-		mv $i_utf8.csv $i.csv
+		iconv -f ISO-8859-1 $i -t UTF-8 -o $i_utf8.tab
+		rm $i.tab
+		mv $i_utf8.tab $i.tab
 	done
 done
 cd ..
