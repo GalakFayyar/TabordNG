f63698bdff1553b6b39f130b466f9e843c5ad05c
Thomas Falcher <thomasfalcher@MacBookPro-2.local>
2016-09-26 20:38:58 +0200

    DEBUG

--- batchs/tools/swallow/swallow/inout/Postgresqlio.py
+++ batchs/tools/swallow/swallow/inout/Postgresqlio.py
@@ -149,7 +149,7 @@ class PostgreSqlIo:
                             update_sql=update_sql,
                             insert_sql=insert_sql
                         )
-                    logger.debug(sql)
+                    logger.info(sql)
                     cursor.execute(sql)
                 except psycopg2.Error as e:
                     with self.counters['nb_items_error'].get_lock():
