12e1a7a5399131fd6b7c8b921e2033bab8df836f
Thomas Falcher <thomasfalcher@MacBookPro-2.local>
2016-10-06 21:37:29 +0200

    DEBUG

--- batchs/tools/swallow/swallow/inout/Postgresqlio.py
+++ batchs/tools/swallow/swallow/inout/Postgresqlio.py
@@ -39,7 +39,6 @@ class PostgreSqlIo:
             dbpass=self.password
         )
         connection = psycopg2.connect(connection_string)
-        print("Connection opened")
 
         try:
             offset = p_start
@@ -48,6 +47,7 @@ class PostgreSqlIo:
             query = p_query
             if query.endswith(';'):
                 query = query[:-1]
+                print(query)
             with connection.cursor(cursor_factory=RealDictCursor) as cursor:
                 while not stop:
                     paginated_query = "{0} limit {1},{2}".format(p_query, offset, p_bulksize)
