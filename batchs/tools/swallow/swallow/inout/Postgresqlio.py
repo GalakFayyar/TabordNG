232128729c079bcfc3ea5ad76b755fc4066e953b
Thomas Falcher <thomasfalcher@MacBookPro-2.local>
2016-10-06 21:38:08 +0200

    DEBUG

--- batchs/tools/swallow/swallow/inout/Postgresqlio.py
+++ batchs/tools/swallow/swallow/inout/Postgresqlio.py
@@ -45,9 +45,9 @@ class PostgreSqlIo:
             stop = False
             # delete ";" if set at the end of the query
             query = p_query
+            print(query)
             if query.endswith(';'):
                 query = query[:-1]
-                print(query)
             with connection.cursor(cursor_factory=RealDictCursor) as cursor:
                 while not stop:
                     paginated_query = "{0} limit {1},{2}".format(p_query, offset, p_bulksize)
