519fc33ff196e9f3c88b5cdcadef9d0985d8a032
Thomas Falcher <thomasfalcher@MacBookPro-2.local>
2016-10-19 21:05:14 +0200

    DEBUG

--- batchs/custom_data_enrichment.py
+++ batchs/custom_data_enrichment.py
@@ -65,8 +65,6 @@ def aggregate_data_pharmacie(p_connection, p_cursor, p_list_pharmacie_id, p_sql_
             p_cursor.execute(p_sql_query, args)
             sql_result_new_ventes = p_cursor.fetchall()
 
-            logger.info(sql_result_new_ventes)
-
             # Récupération des données de ventes actuelles pour la pharamacie
             p_cursor.execute("SELECT * FROM ventes_pharmacies_periodes WHERE idPharmacie = %s;", args)
             sql_result_ventes = p_cursor.fetchone()
@@ -146,6 +144,8 @@ def aggregate_data_pharmacie(p_connection, p_cursor, p_list_pharmacie_id, p_sql_
                 'ventes_p4': json.dumps(obj_vente_p4).strip(),
                 'ventes_p5': json.dumps(obj_vente_p5).strip()
             }
+
+            print(result)
 
             update_document(p_cursor=p_cursor, source_doc=result, p_id_field='idpharmacie', p_table='ventes_pharmacies_periodes')
 
