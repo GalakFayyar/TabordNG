667ce7598043e52ec843324a6fda20294d9220bc
Thomas Falcher <thomasfalcher@MacBookPro-2.local>
2016-10-21 19:35:31 +0200

    Adjust tool methods for psycopg2 v2.4.5 and v2.6.2

--- api/resources/tools.py
+++ api/resources/tools.py
@@ -1,7 +1,6 @@
 from logger import logger
-#from resources.tools import *
 
-import datetime, time, pytz, json
+# import json # importer si psycopg2 v2.4.5
 
 class Tools():
 
@@ -9,7 +8,8 @@ class Tools():
         # return [{'id': elt['id'], 'data': json.loads(elt['data'])} for elt in psql_result]
         _results = []
         for elt in psql_result:
-            _temp_elt = json.loads(elt['data'])
+            #_temp_elt = json.loads(elt['data']) # ok avec psycopg2 v2.4.5 -> elt['data'] = str
+            _temp_elt = elt['data'] # ok avec psycopg2 v.2.6.2 -> elt['data'] = dict
             _temp_elt['id'] = elt['id']
             _results.append(_temp_elt)
 
@@ -18,7 +18,8 @@ class Tools():
     def format_complex_psql_result(self, psql_result, fields_array, json_field):
         _results = []
         for elt in psql_result:
-            _temp_elt = {json_field: json.loads(elt['data'])}
+            # _temp_elt = {json_field: json.loads(elt['data'])} # ok avec psycopg2 v2.4.5 -> elt['data'] = str
+            _temp_elt = {json_field: elt['data']} # ok avec psycopg2 v.2.6.2 -> elt['data'] = dict
             for field in fields_array:
                 _temp_elt[field] = str(elt[field])
             _results.append(_temp_elt)
