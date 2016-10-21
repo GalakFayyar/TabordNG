bc16edeb2efabd2c6739dc5d3d22b64ffd1658f1
Thomas Falcher <thomasfalcher@MacBookPro-2.local>
2016-10-21 23:58:29 +0200

    DEBUG

--- api/resources/vente.py
+++ api/resources/vente.py
@@ -38,8 +38,8 @@ class Vente():
 
         for period in ['period1', 'period2', 'period3', 'period4', 'period5']:
             logger.debug(sql_result[0])
-            if period in sql_result[0] and len(json.load(sql_result[0][period])) > 0:
-                result = json.load(sql_result[0][period])
+            if period in sql_result[0] and len(json.loads(sql_result[0][period])) > 0:
+                result = json.loads(sql_result[0][period])
 
         logger.debug(result)
 
